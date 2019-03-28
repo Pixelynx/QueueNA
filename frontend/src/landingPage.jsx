5ce21e2103a80970fec6d8e946ac15394219e87c
Deyvi <deyvio@gmail.com>
2019-03-28 14:19:02 -0400

    added new user creation + user can participate in chat

--- frontend/src/landingPage.jsx
+++ frontend/src/landingPage.jsx
@@ -1,51 +1,41 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 
-import './styles/landingPage.css';
+import "./styles/landingPage.css";
 
 class LandingPage extends Component {
-  state = {
-    selectedChannel: '',
-    currentUser: ''
-  }
-
-
-  // intended to grab value of selection to use as params in link -- probably need async function
-  handleChannelSelect = (e) => {
-    this.setState({ selectedChannel: e.target.value })
-  }
-
-  handleTempUser = (e) => {
-    this.setState({ currentUser: e.target.value })
-  }
-
   render() {
-    console.log(this.state)
-    return(
+    return (
       <>
-      <div className="landing_container">
-      <title>Queue&A</title>
+        <div className="landing_container">
+          <title>Queue&A</title>
 
           <h1>Welcome to Queue&A</h1>
 
-          <h2>An app for developers to help other developers troubleshoot and problem solve.</h2>
+          <h2>
+            An app for developers to help other developers troubleshoot and
+            problem solve.
+          </h2>
 
-        <p>Enter Username:</p>
+          <p>Enter Username:</p>
 
-        <form className="username" onSubmit={this.handleTempUser}>
-          <input type="text" name='username' />
-        </form>
+          <form className="username" onSubmit={this.props.handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              value={this.props.newUser}
+              onChange={this.props.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
 
-        <p>Please select which channel you'd like to enter:</p>
-
-
-        <a className="channel-btn" href="/channel">Javascript</a>
-        <a className="channel-btn" href="/channel">Swift</a>
-
+          <p>Please select which channel you'd like to enter:</p>
+          <Link to={"/channel"} className="channel-btn">
+            Javascript
+          </Link>
         </div>
-
-        </>
-          )
+      </>
+    );
   }
 }
 
