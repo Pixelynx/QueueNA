471d6a049471c95e9ef8bc85d402bed208f86f04
pixelynx <tjstevens1992@gmail.com>
2019-03-28 08:29:42 -0400

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import MessageContainer from './messaging/messageContainer.jsx';
-import LandingPage from './messaging/landingPage.jsx';
+import LandingPage from './landingPage.jsx';
 
 class App extends Component {
   render() {
     return(
       <>
-        <MessageContainer />
-
         <Route exact path='/' component={ LandingPage }/>
+        <Route path='/channel' component={ MessageContainer } />
+        <Route path='/about' />
       </>
     )
   }
--- frontend/src/landingPage.jsx
+++ frontend/src/landingPage.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react';
+
+import './styles/landingPage.css';
 
 class LandingPage extends Component {
   render() {
     return(
-      
-    )
+      <>
+      <div class="landing_container">
+      <title>Queue&A</title>
+
+          <h1>Welcome to Queue&A</h1>
+
+          <h2>An app for developers to help other developers troubleshoot and problem solve.</h2>
+
+        <p>Enter Username:</p>
+
+        <form className="username">
+          <input type="text" name='username' />
+        </form>
+
+        <p>Please select which channel you'd like to enter:</p>
+
+
+        <a class="channel-btn" href="/channel">Javascript</a>
+        <a class="channel-btn" href="/channel">Swift</a>
+
+        </div>
+
+        </>
+          )
   }
 }
 
