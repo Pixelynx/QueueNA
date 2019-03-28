1b56dc9c9d8c71cb74c62342dd58cbce88eb3c2c
pixelynx <tjstevens1992@gmail.com>
2019-03-27 22:50:41 -0400

    fixing errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-import Config from './config.jsx';
+import ChatApp from './chatapp.jsx';
 import './App.css';
 
+import { default as Chatkit } from '@pusher/chatkit-client';
+
+const tokenProvider = new Chatkit.TokenProvider({
+  url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0d111987-7cc9-43a5-944a-628ce39f9dff/token"
+});
+
+const chatManager = new Chatkit.ChatManager({
+  instanceLocator: "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff",
+  userId: "tjstevens92",
+  tokenProvider: tokenProvider
+});
+
 class App extends Component {
-  render() {
-
-    return (
-      <div className="App">
-
-      </div>
-    );
+  constructor(props) {
+      super(props);
+      this.state = {
+        currentView: 'ChatMessage'
+      }
+      this.changeView = this.changeView.bind(this);
   }
-}
 
+  changeView(view) {
+      this.setState({
+          currentView: view
+      })
+  }
+
+  render() {
+        let view ='';
+        if (this.state.currentView === "ChatMessage") {
+            view = <ChatMessage  changeView={this.changeView}/>
+        }
+        return (
+            <div className="App">
+                {view}
+            </div>
+        );
+    }    
+}
 export default App;
