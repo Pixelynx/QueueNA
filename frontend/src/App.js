0add6790f37e34a5bae7aa0c4a36f5ad00e68d3a
pixelynx <tjstevens1992@gmail.com>
2019-03-28 14:59:43 -0400

    get room working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,87 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import MessageContainer from './messaging/messageContainer.jsx';
-import LandingPage from './landingPage.jsx';
-import { About } from './about.jsx';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import MessageContainer from "./messaging/messageContainer.jsx";
+import LandingPage from "./landingPage.jsx";
+import { About } from "./about.jsx";
 
-import './App.css';
+import Chatkit from "@pusher/chatkit-server";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    newUser: "",
+    isNewUserCreated: false,
+    chatkit: ""
+  };
+
+  componentDidMount() {
+    const chatkit = new Chatkit({
+      instanceLocator: "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff",
+      key:
+        "a48470b6-48ae-41cb-8a79-2f51d22a0fe5:9NCpwiAJ1y8ecgzM2iQFUgJinrNC1+dcX8qWSwBPbqw="
+    });
+
+    this.setState({
+      chatkit: chatkit
+    });
+  }
+
+  handleNewUser = () => {
+    this.state.chatkit
+      .createUser({
+        id: this.state.newUser,
+        name: this.state.newUser
+      })
+      .then(res => {
+        console.log("User created successfully");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+
+  handleChange = e => {
+    this.setState({ newUser: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ isNewUserCreated: true });
+    this.handleNewUser();
+  };
+
   render() {
-    return(
+    console.log(this.state);
+    return (
       <>
-        <Route exact path='/' component={ LandingPage }/>
-        <Route path='/channel/:name' component={ MessageContainer } />
-        <Route path='/about' component={ About }/>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <LandingPage
+              handleChange={this.handleChange}
+              newUser={this.state.newUser}
+              handleSubmit={this.handleSubmit}
+            />
+          )}
+        />
+        <Route
+          path="/channel/"
+          render={() => (
+            <MessageContainer
+              chatkitServer={this.state.chatkit}
+              username={
+                this.state.isNewUserCreated ? this.state.newUser : "Anonymous"
+              }
+            />
+          )}
+        />
+        <Route path="/about" component={About} />
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- frontend/src/messaging/messageContainer.jsx
+++ frontend/src/messaging/messageContainer.jsx
@@ -1,74 +1,102 @@
-import React, { Component } from 'react';
-import Messages from './messages.jsx';
-import SendMessage from './sendMessage.jsx';
-import Channels from './channels.jsx';
-// import { ChatManager, TokenProvider } from "@pusher/chatkit-server";
+import React, { Component } from "react";
+import Messages from "./messages.jsx";
+import SendMessage from "./sendMessage.jsx";
+import Channels from "./channels.jsx";
 import Chatkit from "@pusher/chatkit-client";
+import ChatkitServer from "@pusher/chatkit-server";
 
-import '../styles/chatbox.css';
+import "../styles/chatbox.css";
 
-const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0d111987-7cc9-43a5-944a-628ce39f9dff/token"
-const instanceLocator = "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff"
+const testToken =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0d111987-7cc9-43a5-944a-628ce39f9dff/token";
+const instanceLocator = "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff";
 
 class MessageContainer extends Component {
-      state = {
-            messages: [],
-            roomId: '31189269',
-            currentUser: 'tjstevens92'
+  state = {
+    messages: [],
+    rooms: [
+      {id: '31189269', name: 'javascript'},
+      {id: '31189404', name: 'swift'},
+      {id: '31189405', name: 'java'}
+    ],
+    roomId: "31189269",
+    chatkitServer: ""
+  };
+
+
+  // Maybe we can connect [props] the currentUser to the username form on the landing page
+
+  componentDidMount = () => {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: this.props.username,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    });
+
+    chatManager.connect().then(currentUser => {
+      this.setState({ currentUser: currentUser });
+      return currentUser.subscribeToRoom({
+        roomId: this.state.roomId,
+        messageLimit: 100,
+        hooks: {
+          onMessage: message => {
+            this.setState({
+              messages: [...this.state.messages, message]
+            });
+          }
         }
+      });
+    });
 
-    // Maybe we can connect [props] the currentUser to the username form on the landing page
+      const chatkitServer = new ChatkitServer({
+        instanceLocator: "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff",
+        key:
+          "a48470b6-48ae-41cb-8a79-2f51d22a0fe5:9NCpwiAJ1y8ecgzM2iQFUgJinrNC1+dcX8qWSwBPbqw="
+      });
 
-    componentDidMount = () => {
-        const chatManager = new Chatkit.ChatManager({
-            instanceLocator: instanceLocator,
-            userId: 'tjstevens92',
-            tokenProvider: new Chatkit.TokenProvider({
-                url: testToken
-            })
-        })
+      this.setState({
+        chatkitServer: chatkitServer
+      });
 
-        chatManager.connect()
-        .then(currentUser => {
-                    this.setState({ currentUser: currentUser })
-                    return currentUser.subscribeToRoom({
-                        roomId: this.state.roomId,
-                        messageLimit: 100,
-                        hooks: {
-                            onMessage: message => {
-                                this.setState({
-                                    messages: [...this.state.messages, message],
-                                })
-                            },
-                        }
-                    })
-                })
-    }
+  };
 
-    sendMessage = (text) => {
-        this.state.currentUser.sendMessage({
-            text,
-            roomId: this.state.roomId
-        })
-    }
+  getRoom = () => {
+    debugger
+    this.state.chatkitServer.getRoom({
+      roomId: '31189269'
+    })
+    .then(room => console.log('got room', room))
+    .catch(err => console.error(err))
+  }
 
-    render() {
-      console.log(this.state.messages)
-        return (
-          <>
-          <div className='chatbox-container'>
-            <Channels />
-            <div className="message-container">
-              <Messages
-                  roomId={this.state.roomId}
-                  messages={this.state.messages} />
-              <SendMessage
-                  sendMessage={this.sendMessage} />
-            </div>
-            </div>
-          </>
-        );
-    }
+  sendMessage = text => {
+    this.state.currentUser.sendMessage({
+      text,
+      roomId: this.state.roomId
+    });
+  };
+
+  render() {
+
+    console.log(this.state.messages);
+    return (
+      <>
+        <div className="chatbox-container">
+          <Channels />
+          <div className="message-container">
+            <Messages
+              roomId={this.state.roomId}
+              messages={this.state.messages}
+            />
+            <SendMessage sendMessage={this.sendMessage} />
+            <button onClick={this.getRoom.bind(this)}>click</button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default MessageContainer;
