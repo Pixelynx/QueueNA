91ed7d6d67beb955e6f7e730409d8a45e1113693
pixelynx <tjstevens1992@gmail.com>
2019-03-28 00:02:29 -0400

    YUS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,67 @@
 import React, { Component } from 'react';
-import ChatApp from './chatapp.jsx';
-import './App.css';
+import Messages from './messages.jsx';
+import SendMessage from './sendMessage.jsx';
+// import { ChatManager, TokenProvider } from "@pusher/chatkit-server";
+import Chatkit from "@pusher/chatkit-client";
 
-import { default as Chatkit } from '@pusher/chatkit-client';
+import './App.css'
 
-const tokenProvider = new Chatkit.TokenProvider({
-  url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0d111987-7cc9-43a5-944a-628ce39f9dff/token"
-});
-
-const chatManager = new Chatkit.ChatManager({
-  instanceLocator: "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff",
-  userId: "tjstevens92",
-  tokenProvider: tokenProvider
-});
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0d111987-7cc9-43a5-944a-628ce39f9dff/token"
+const instanceLocator = "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff"
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        currentView: 'ChatMessage'
-      }
-      this.changeView = this.changeView.bind(this);
-  }
-
-  changeView(view) {
-      this.setState({
-          currentView: view
-      })
-  }
-
-  render() {
-        let view ='';
-        if (this.state.currentView === "ChatMessage") {
-            view = <ChatMessage  changeView={this.changeView}/>
+      state = {
+            messages: [],
+            roomId: '31189269',
+            currentUser: 'tjstevens92'
         }
+
+
+    componentDidMount = () => {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: instanceLocator,
+            userId: 'tjstevens92',
+            tokenProvider: new Chatkit.TokenProvider({
+                url: testToken
+            })
+        })
+
+        chatManager.connect()
+        .then(currentUser => {
+                    this.setState({ currentUser: currentUser })
+                    return currentUser.subscribeToRoom({
+                        roomId: this.state.roomId,
+                        messageLimit: 100,
+                        hooks: {
+                            onMessage: message => {
+                                this.setState({
+                                    messages: [...this.state.messages, message],
+                                })
+                            },
+                        }
+                    })
+                })
+    }
+
+    sendMessage = (text) => {
+        this.state.currentUser.sendMessage({
+            text,
+            roomId: this.state.roomId
+        })
+    }
+
+    render() {
+      console.log(this.state.messages)
         return (
-            <div className="App">
-                {view}
+            <div className="app">
+              <Messages
+                  roomId={this.state.roomId}
+                  messages={this.state.messages} />
+              <SendMessage
+                  sendMessage={this.sendMessage} />
             </div>
         );
-    }    
+    }
 }
-export default App;
+
+export default App
---
+++ frontend/src/sendMessage.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class SendMessage extends Component {
+        state = {
+            message: ''
+        }
+
+    handleChange = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.sendMessage(this.state.message)
+        this.setState({
+            message: ''
+        })
+    }
+
+    render() {
+      console.log(this.state)
+        return (
+            <form
+                onSubmit={this.handleSubmit}
+                className="send-message-form">
+                <input
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit ENTER"
+                    type="text" />
+            </form>
+        )
+    }
+}
+
+export default SendMessage
