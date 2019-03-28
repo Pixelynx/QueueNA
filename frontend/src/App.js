d8fa930d5520c69f894d66c8f2a96746bd8e0727
pixelynx <tjstevens1992@gmail.com>
2019-03-28 00:22:57 -0400

    cleaning up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,14 @@
 import React, { Component } from 'react';
-import Messages from './messages.jsx';
-import SendMessage from './sendMessage.jsx';
-// import { ChatManager, TokenProvider } from "@pusher/chatkit-server";
-import Chatkit from "@pusher/chatkit-client";
-
-import './App.css'
-
-const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0d111987-7cc9-43a5-944a-628ce39f9dff/token"
-const instanceLocator = "v1:us1:0d111987-7cc9-43a5-944a-628ce39f9dff"
+import MessageContainer from './messaging/messageContainer.jsx';
+import Messages from './messaging/messages.jsx';
 
 class App extends Component {
-      state = {
-            messages: [],
-            roomId: '31189269',
-            currentUser: 'tjstevens92'
-        }
-
-
-    componentDidMount = () => {
-        const chatManager = new Chatkit.ChatManager({
-            instanceLocator: instanceLocator,
-            userId: 'tjstevens92',
-            tokenProvider: new Chatkit.TokenProvider({
-                url: testToken
-            })
-        })
-
-        chatManager.connect()
-        .then(currentUser => {
-                    this.setState({ currentUser: currentUser })
-                    return currentUser.subscribeToRoom({
-                        roomId: this.state.roomId,
-                        messageLimit: 100,
-                        hooks: {
-                            onMessage: message => {
-                                this.setState({
-                                    messages: [...this.state.messages, message],
-                                })
-                            },
-                        }
-                    })
-                })
-    }
-
-    sendMessage = (text) => {
-        this.state.currentUser.sendMessage({
-            text,
-            roomId: this.state.roomId
-        })
-    }
-
-    render() {
-      console.log(this.state.messages)
-        return (
-            <div className="app">
-              <Messages
-                  roomId={this.state.roomId}
-                  messages={this.state.messages} />
-              <SendMessage
-                  sendMessage={this.sendMessage} />
-            </div>
-        );
-    }
+  render() {
+    return(
+      <>
+      </>
+    )
+  }
 }
 
 export default App
